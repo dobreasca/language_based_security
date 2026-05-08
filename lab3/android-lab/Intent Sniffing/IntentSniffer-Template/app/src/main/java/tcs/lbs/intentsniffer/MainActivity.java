ddb6de1d5cdc81a0a54d7231cbc65ace3d94ace6
Aurora Navas <73229219+auroranavas@users.noreply.github.com>
2026-05-08 19:54:48 +0200

    Implement location broadcast receiver in MainActivity

--- lab3/android-lab/Intent Sniffing/IntentSniffer-Template/app/src/main/java/tcs/lbs/intentsniffer/MainActivity.java
+++ lab3/android-lab/Intent Sniffing/IntentSniffer-Template/app/src/main/java/tcs/lbs/intentsniffer/MainActivity.java
@@ -3,11 +3,19 @@ package tcs.lbs.intentsniffer;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.os.Build;
 import android.widget.TextView;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.location.Location;
 
 public class MainActivity extends AppCompatActivity
 {
     TextView LatitudeTextView, LongitudeTextView;
+
+    SnifferReceiver sniffer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
@@ -19,8 +27,43 @@ public class MainActivity extends AppCompatActivity
         LongitudeTextView = findViewById(R.id.LongitudeTextView);
 
 
-        // TODO Define a broadcast receiver class
         // TODO Register to receive broadcast messages from ForegroundLocationService of the LocationApp
         // TODO Extract Location coordinates from the broadcast message and show them in LatitudeTextView and LongitudeTextView
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        IntentFilter broadcastFilter = new IntentFilter();
+
+        broadcastFilter.addAction("tcs.lbs.locationapp.MainActivityReceiver");
+        broadcastFilter.addAction("tcs.lbs.weather_app.WeatherBroadcastReceiver");
+
+        sniffer = new SnifferReceiver();
+
+        registerReceiver(sniffer, broadcastFilter, Context.RECEIVER_EXPORTED);
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if (sniffer != null) {
+            unregisterReceiver(sniffer);
+        }
+    }
+
+    public class SnifferReceiver extends BroadcastReceiver {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            Location interceptedLocation = intent.getParcelableExtra("Location");
+
+            if (interceptedLocation != null) {
+                LatitudeTextView.setText("Lat: " + interceptedLocation.getLatitude());
+                LongitudeTextView.setText("Lon: " + interceptedLocation.getLongitude());
+            }
+        }
+    }
+
 }
